2b3ddefc9dd9bf2da89011317c630e38f9649022
somkiat <somkiat.p@gmail.com>
2016-05-09 06:30:37 +0700

    Add UI testing with espresso

---
+++ demo/DemoUnitTest/app/src/androidTest/java/demo/somkiat/demounittest/CalculatorAddTest.java
@@ -0,0 +1,40 @@
+package demo.somkiat.demounittest;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CalculatorAddTest {
+
+    public static final String ONE = "1";
+    public static final String TWO = "2";
+    public static final String RESULT = "3.0";
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void calculatorAdd() {
+        onView(withId(R.id.operand_one_edit_text)).perform(typeText(ONE));
+        onView(withId(R.id.operand_two_edit_text)).perform(typeText(TWO));
+        onView(withId(R.id.operation_add_button)).perform(click());
+        onView(withId(R.id.operation_result_text_view)).check(matches(withText(RESULT)));
+    }
+
+}
+
+
--- demo/DemoUnitTest/app/src/main/java/demo/somkiat/demounittest/MainActivity.java
+++ demo/DemoUnitTest/app/src/main/java/demo/somkiat/demounittest/MainActivity.java
@@ -2,12 +2,56 @@ package demo.somkiat.demounittest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Calculator mCalculator;
+
+    private EditText mOperandOneEditText;
+    private EditText mOperandTwoEditText;
+
+    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCalculator = new Calculator();
+        mResultTextView = (TextView) findViewById(R.id.operation_result_text_view);
+        mOperandOneEditText = (EditText) findViewById(R.id.operand_one_edit_text);
+        mOperandTwoEditText = (EditText) findViewById(R.id.operand_two_edit_text);
+    }
+
+    public void onAdd(View view) {
+        double operandOne;
+        double operandTwo;
+        try {
+            operandOne = getOperand(mOperandOneEditText);
+            operandTwo = getOperand(mOperandTwoEditText);
+        } catch (NumberFormatException nfe) {
+            mResultTextView.setText(getString(R.string.computationError));
+            return;
+        }
+
+        String result = String.valueOf(mCalculator.add(operandOne, operandTwo));
+        mResultTextView.setText(result);
+    }
+
+    private static Double getOperand(EditText operandEditText) {
+        String operandText = getOperandText(operandEditText);
+        return Double.valueOf(operandText);
+    }
+
+    private static String getOperandText(EditText operandEditText) {
+        String operandText = operandEditText.getText().toString();
+        if (TextUtils.isEmpty(operandText)) {
+            throw new NumberFormatException("operand cannot be empty!");
+        }
+        return operandText;
     }
 }
